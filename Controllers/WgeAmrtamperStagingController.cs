8c2687d7a151c6aef094c486e94f09bf97fefe86
djdagostino <152912672+daviddagWGELD@users.noreply.github.com>
2024-01-03 15:13:17 -0500

    Local SQL DB Indexed

---
+++ Controllers/WgeAmrtamperStagingController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TestClusterProj.Models;
+
+namespace TestClusterProj.Controllers
+{
+    public class WgeAmrtamperStagingController : Controller
+    {
+        private readonly ClusterContext _context;
+
+        public WgeAmrtamperStagingController(ClusterContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var data = await _context.WgeAmrtamperStagings.ToListAsync();
+            return View(data);
+        }
+    }
+}
